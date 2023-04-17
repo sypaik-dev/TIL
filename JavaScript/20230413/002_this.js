a1fb95e5794eee06cfecee67f05cb158c4cdba3e
sypaik.dev@gmail.com <sypaik.dev@gmail.com>
2023-04-17 22:04:13 +0900

    2023.04.17

--- JavaScript/20230413/002_this.js
+++ JavaScript/20230413/002_this.js
@@ -267,3 +267,37 @@ function a() {
 a();
 // a, b, c모두 window
 // 위 함수는 a.b.c()이런 식으로 호출한 객체를 타고 올라가는 형태가 아닙니다.
+
+function attackBeam() {
+  // 레이저 공격
+  this.hp -= 20;
+}
+
+function attackKnife() {
+  // 칼공격
+  if (this.name === "thanos") {
+    this.hp -= 1;
+    return;
+  }
+  this.hp -= 5;
+}
+
+let jombi = {
+  name: "jombi",
+  damaged1: attackBeam,
+  damaged2: attackKnife,
+  hp: 10000,
+  power: 2,
+};
+
+let thanos = {
+  name: "thanos",
+  damaged1: attackBeam,
+  damaged2: attackKnife,
+  hp: 1000,
+  power: 100,
+};
+
+jombi.damaged1(); // Beam
+jombi.damaged2(); // Knife
+jombi.hp;
