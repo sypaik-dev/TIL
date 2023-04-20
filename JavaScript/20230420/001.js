fd774bfb297e622056d1e515abff1f0855321a72
sypaik.dev@gmail.com <sypaik.dev@gmail.com>
2023-04-21 01:04:35 +0900

    2023.04.20

--- JavaScript/20230420/001.js
+++ JavaScript/20230420/001.js
@@ -29,3 +29,49 @@ const sausage1 = new Sausage("소고기", "파");
 sausage1.taste();
 const sausage2 = new FireSausage("소고기", "파");
 sausage2.taste();
+
+/* 강사님 코드 */
+class Sausage {
+  constructor(el1, el2) {
+    this.inside1 = el1;
+    this.inside2 = el2;
+  }
+
+  taste() {
+    console.log(`와~~${this.inside1}맛과 ${this.inside2}맛이 나는 소세지다!!`);
+  }
+}
+
+class FireSausage extends Sausage {
+  constructor(el1, el2, el3) {
+    // this.inside1 = el1;
+    // this.inside2 = el2;
+    super(el1, el2);
+    this.inside3 = el3;
+  }
+
+  // 오버로딩
+  // taste(el1) {
+  //     console.log(`와~~${this.inside1}맛과 ${this.inside2}맛과 ${this.inside3}맛이 나는 소세지다!!`);
+  // }
+
+  // taste(el1, el2) {
+  //     console.log(`와~~${this.inside1}맛과 ${this.inside2}맛과 ${this.inside3}맛이 나는 소세지다!!`);
+  // }
+
+  taste(el1) {
+    if (el1) {
+      console.log(
+        `와~~${this.inside1}맛과 ${this.inside2}맛과 ${this.inside3}맛이 나는 소세지다!!`
+      );
+    } else {
+    }
+  }
+
+  // 오버라이딩
+  // taste() {
+  //     console.log(`와~~${this.inside1}맛과 ${this.inside2}맛과 ${this.inside3}맛이 나는 소세지다!!`);
+  // }
+}
+
+const fireSausage = new FireSausage("파맛", "불맛", "키위맛");
