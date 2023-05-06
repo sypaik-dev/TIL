1e1eb58eaa4d7ccd585e9eeb12b12f175637aba9
sypaik.dev@gmail.com <sypaik.dev@gmail.com>
2023-05-06 22:17:34 +0900

    2023.05.06

---
+++ JavaScript/20230506/timeCheck.js
@@ -0,0 +1,67 @@
+let start = Date.now();
+
+const result = (x) => {
+    console.log(`${x}가 실행되는데 걸리는 시간 : ${Date.now() - start}`);
+}
+
+const asyncBlocking = () => {
+    return new Promise((resolve) => {
+        let i = 0;
+        while (i < 9999999) {
+            i++
+        }
+        resolve("blocking finished");
+    });
+}
+
+result('동기식 코드1');
+
+setTimeout(() => {
+    result('setTimeout finished');
+}, 0);
+
+// 프로미스 객체를 만드는 과정은 동기적으로 진행됩니다!
+// result(asyncBlocking());
+
+result(asyncBlocking().then((txt) => {
+    result(txt);
+}));
+
+result('동기식 코드2');
+
+function message() {
+
+    Promise.all([
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('hello');
+            }, 100)
+        }),
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('world');
+            }, 100)
+        })
+    ]).then(([hello, world]) => {
+        console.log(`${hello} ${world}`);
+    });
+}
+
+async function message() {
+    const [hello, world] = await Promise.all([
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('hello');
+            }, 100)
+        }),
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('world');
+            }, 100)
+        })
+    ]);
+
+    console.log(`${hello} ${world}`);
+}
+
+message();
