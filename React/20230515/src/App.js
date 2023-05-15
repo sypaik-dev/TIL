430eb2606335e2ea4361b4369f3a4c0b1612a376
sypaik.dev@gmail.com <sypaik.dev@gmail.com>
2023-05-15 14:15:27 +0900

    리액트 UseEffect

---
+++ React/20230515/src/App.js
@@ -0,0 +1,20 @@
+import RouteStudy from './RouterSolve/RouteStudy';
+import RouteSolve from './RouterSolve/RouteSolve';
+import Counter from './HookUseEffect/TryUseEffect';
+import Time from './HookUseEffect/Quiz2'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <RouteStudy/> */}
+      {/* <RouteSolve/> */}
+      {/* <Counter/>  */}
+       <Time/>
+    </div>
+  );
+}
+
+export default App;
+
+
